e55b9502b0d64606a68c06acaa8393f31574c00e
Wesley Ricketts <wricketts@brandeis.edu>
2023-07-18 15:22:20 -0400

    Modify process.py to read annotations.tab files in the batch directory and generate gold data in .tsv format containing one file per media GUID.
    Replace gold data with the new output files.
    
    Add documentation about gold data to readme.
    
    Update .gitignore

--- newshour-namedentity-wikipedialink/process.py
+++ newshour-namedentity-wikipedialink/process.py
@@ -1,34 +1,61 @@
-"""Processing NER uploads.
+"""Processes Named Entity Linking annotation files
 
-Creates MMIF or CoNNL files from the Brat annotation files.
+$ process.py [-h] PROJECT_DIR
 
-$ process.py [-h] [--export] PROJECT_DIR
-
-Takes all  annotations file in the 'PROJECT_DIR/annotations' directory and
-copy them to the working directory.
-
-With the --export option the results are then copied to the "gold" directory at
-the toplevel of this repository, where they are ready to be committed back into
-the annotation collection repository and pushed up. The script does not do the
-automatic commit and push in order to avoid hasty commits.
+Reads in each '.tab' annotation file in the 'PROJECT_DIR' directory.
+Fetches wikidata QIDs using the wikipedia URLs in the data.
+Generates a tsv file for each unique GUID. Exports the results to a "gold" directory
+at the top level of this repository, where they are ready to be committed back
+into the annotation collection repository and pushed up. The script does not do
+the automatic commit and push in order to avoid hasty commits.
 
 """
 
-from pathlib import Path
-import shutil
 import argparse
+import pandas as pd
+from pathlib import Path
+import requests
+from typing import List, Union
+from urllib.parse import unquote_plus
 
 
-ANNOTATIONS_DIR = Path('./annotations')
-WORKING_DIR = Path('./working')
+def fetch_wikidata_qids(urls: Union[str, List[str]]) -> Union[str, List[str]]:
+    """
+    Fetch wikidata QIDs from English Wikipedia URLs.
+    """
+    api = "https://en.wikipedia.org/w/api.php?"
+    # headers
+    reqheaders = {'Accept': 'application/json'}
+    if isinstance(urls, list):
+        titles = [unquote_plus(url.split("/")[-1]) for url in urls]
+        titles = " | ".join(titles)
+    else:
+        titles = unquote_plus(urls.split("/")[-1])
+    # parameters to use
+    params = {
+        'action': 'query',
+        'prop': 'pageprops',
+        'ppprop': 'wikibase_item',
+        'titles': titles,
+        'redirects': 1,
+        'format': 'json'
+    }
+    json_data = requests.post(api, data=params, headers=reqheaders).json()
+    # parse resulting json for QIDs
+    qids = []
+    for page in json_data['query']['pages'].values():
+        qid = page['pageprops']['wikibase_item']
+        qids.append("https://www.wikidata.org/wiki/" + qid)
+    if len(qids) > 1:
+        return list(reversed(qids))
+    else:
+        return qids[0]
 
 
 def parse_arguments():
     ap = argparse.ArgumentParser(
-        description='Process uploaded NEL annotation files with named entitie links')
-    ap.add_argument('batch', help='Batch to process files for')
-    ap.add_argument('--export', default=False, action='store_true',
-                    help='Export results to the gold directory')
+        description='Process uploaded NER annotation files with named entity links')
+    ap.add_argument('batch', help='Directory containing the batch the process')
     return ap.parse_args()
 
 
@@ -37,24 +64,21 @@ if __name__ == '__main__':
     options = parse_arguments()
 
     batch_dir = Path(options.batch).resolve()
-    annotations_dir = batch_dir / 'annotations'
-    working_dir = batch_dir / 'working'
-    working_dir.mkdir(exist_ok=True)
 
-    # this is overkill because we do not even change the file, but we may do
-    # that in the future
-    print(f'>>> Copying files...')
-    print(f'>>> {annotations_dir}')
-    print(f'>>> --> {working_dir}')
-    for tab_file in annotations_dir.glob('*.tab'):
-        shutil.copy(str(tab_file), working_dir)
-
-    if options.export:
-        batch = Path(options.batch).resolve().name
-        repo_dir = (Path(__file__).parent / '..' / '..').resolve()
-        gold_dir = repo_dir / 'golds' / 'nel' / batch
-        gold_dir.mkdir(parents=True, exist_ok=True)
-        print(f'>>> Exporting {batch} annotations to the gold directory')
-        print(f'>>> --> {gold_dir}')
-        for tab_file in working_dir.glob('*.tab'):
-            shutil.copy(str(tab_file), gold_dir)
+    batch = Path(options.batch).resolve().name
+    repo_dir = (Path(__file__).parent / '..' / '..').resolve()
+    gold_dir = repo_dir / 'golds' / 'nel' / batch
+    gold_dir.mkdir(parents=True, exist_ok=True)
+    print(f'>>> Exporting {batch} annotations to the gold directory')
+    print(f'>>> --> {gold_dir}')
+    for tab_file in batch_dir.glob('*.tab'):
+        with open(tab_file) as fh:
+            df = pd.read_table(fh, sep='\t', encoding='utf-16', header=None)
+        df.dropna(axis=1, how='all', inplace=True)
+        df.columns = ['index', 'timestamp', 'GUID', 'entity', 'type', 'instances', 'wiki_url']
+        df['QID'] = df['wiki_url'].map(fetch_wikidata_qids, na_action='ignore')
+        grouped = df.groupby(['GUID'])
+        unique_guids = list(df["GUID"].unique())
+        for guid in unique_guids:
+            output_df = grouped.get_group(guid)
+            output_df.to_csv(Path(gold_dir, guid.rstrip('-transcript.ann')).with_suffix('.tsv'), sep='\t', index=False)
--- newshour-namedentity-wikipedialink/readme.md
+++ newshour-namedentity-wikipedialink/readme.md
@@ -1,2 +1,14 @@
-The input to this annotation project was from `newshour-namedentity` annotation project in this repository.
+
 ***Needs to be updated***
+
+## Named Entity Linking
+
+### Gold Data Generation
+The input to this annotation project was from the `newshour-namedentity` annotation project in this repository. The `golds` directory contains the public gold dataset, generated using `process.py`. Files are in .tsv format. The relevant data fields are summarized below:
+
+- __GUID__: The AAPB GUID of the annotated transcript. (_string_)
+- __entity__: The span of text containing the entity. (_string_)
+- __type__: The entity category. (_string_)
+- __instances__: The number of instances of the entity within the source file. (_int_)
+- __wiki_url__: The Wikipedia URL grounding the entity. (_string_)
+- __QID__: The Wikidata URI linking the entity via Q identifier. (_string_)
