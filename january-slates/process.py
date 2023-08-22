416dfad184851dc0a92448240f3ce00b9429b777
Keigh Rim <keigh.rim@gmail.com>
2023-08-22 16:28:31 -0400

    updated process.py for slates not to generate non-annotated media

--- january-slates/process.py
+++ january-slates/process.py
@@ -1,6 +1,5 @@
-# To read all the tabular files from in the YYMMDD-batchname directories and generate one file per GUID in golds
-
 """Processes Slate annotation files
+To read all the tabular files from in the YYMMDD-batchname directories and generate one file per GUID in golds
 
 $ process.py --input_path /your/input/path --output_path /your/output/path
 
@@ -17,14 +16,15 @@ def process_csv(input_directory, output_directory):
         "Writing Types", "Recorded/Digital", "format of most of the information",
         "Anything moving on screen during slate?"
     ]
+    batchname = os.path.basename(input_directory)[7:]  # chopping date prefix
     for filename in os.listdir(input_directory):
         if filename.endswith(".csv"):
             filepath = os.path.join(input_directory, filename)
             df = pd.read_csv(filepath,encoding='utf-8')
             df.replace(',', '', regex=True, inplace=True)
             df.columns = [col.replace(',', '') for col in df.columns]
-            df.columns= [col.replace('"', '') for col in df.columns]
-            df.columns= [col.replace(' ', '') for col in df.columns]
+            df.columns = [col.replace('"', '') for col in df.columns]
+            df.columns = [col.replace(' ', '') for col in df.columns]
             df.rename(columns={
                 'SlateStart': 'Slate Start',
                 'SlateEnd': 'Slate End',
@@ -38,16 +38,18 @@ def process_csv(input_directory, output_directory):
             for _, row in df.iterrows():
                 guid = row['GUID']
                 tsv_filename = f"{guid}.tsv"
-                tsv_filepath = os.path.join(output_directory, tsv_filename)
+                tsv_filepath = os.path.join(output_directory, batchname, tsv_filename)
+                os.makedirs(os.path.dirname(tsv_filepath), exist_ok=True)
                 row_df = pd.DataFrame([row[desired_columns]])
-                row_df.to_csv(tsv_filepath, index=False)
+                if row_df['Slate Start'].values[0]:
+                    row_df.to_csv(tsv_filepath, index=False)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input_path', type=str, required=True,
+    parser.add_argument('input_path', type=str,
                         help='Path to the directory containing the CLAMS slate annotation metadata files')
-    parser.add_argument('--output_path', type=str, required=True,
+    parser.add_argument('--output_path', type=str, default='golds',
                         help='Path to the output .tsv file')
     args = parser.parse_args()
     process_csv(args.input_path, args.output_path)
