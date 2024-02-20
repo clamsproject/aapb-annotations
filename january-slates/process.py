e54e714a86bb599debbf3cbeb39727887a6f1a75
Keigh Rim <keigh.rim@gmail.com>
2024-02-20 14:12:37 -0500

    added missing "no slate" annotations to gold set

--- january-slates/process.py
+++ january-slates/process.py
@@ -37,41 +37,46 @@ def process_csv(input_directory, output_directory):
                 csv_filepath = os.path.join(output_directory, csv_filename)
                 os.makedirs(os.path.dirname(csv_filepath), exist_ok=True)
                 row_df = pd.DataFrame([row[desired_columns]])
+                is_annotated = False
+                has_slate = False
                 for time_col in ['start', 'end']:
                     v = row_df[time_col].values[0]
                     if v.startswith('no'):
                         row_df[time_col] = 'NO'
+                        is_annotated = True
                     elif ';' in v:
                         # standard format is 00:00:00.000, which is 12 characters
                         time, frnum = v.split(';')
                         millisecs = int(frnum[:2]) / 30 * 1000
                         row_df[time_col] = f'{time}.{millisecs:03.0f}'
+                        is_annotated = True
+                        has_slate = True
                     elif ':' in v:
                         row_df[time_col] = f'{v}.000'
-                v = row_df['type'].values[0]
-                if pd.isnull(v):
+                        is_annotated = True
+                        has_slate = True
+                if not is_annotated:
+                    print(f"Start or end time not annotated for {row_df['GUID'].values[0]}")
                     continue
+                v = row_df['type'].values[0]
+                if not has_slate:
+                    row_df['type'] = '-'
                 elif v.lower().startswith('hand') or v.lower().startswith('fixed'):
                     row_df['type'] = 'h'
                 elif v.lower().startswith('type'):
                     row_df['type'] = 't'
-                elif len(v) == 0:
-                    continue
                 else:
                     raise ValueError(f"Unknown type: {v}")
                 v = row_df['digital'].values[0]
-                if pd.isnull(v):
-                    continue
+                if not has_slate:
+                    row_df['digital'] = False
                 elif v.lower().startswith('digit'):
                     row_df['digital'] = True
                 elif v.lower().startswith('record'):
                     row_df['digital'] = False
-                # elif len(v) == 0:
-                #     continue
                 else:
                     raise ValueError(f"Unknown type: {v}")
-                if row_df['start'].values[0]:
-                    row_df.to_csv(csv_filepath, index=False, sep=',')
+                row_df.to_csv(csv_filepath, index=False, sep=',')
 
 
 if __name__ == '__main__':
