9ae2e009ca930709d90310873b06397f84b10fe3
Marc Verhagen <marc@cs.brandeis.edu>
2024-06-13 06:00:39 -0400

    Added creation of TimeFrame gold files, some refactoring

--- scene-recognition/process.py
+++ scene-recognition/process.py
@@ -1,13 +1,16 @@
 # process script to copy raw csv into new golds directory
 # iterate through each raw directory (use yy-mm-dd pattern to find directories)
-# copy each csv file to golds directory
+# copy each csv file to golds/timepoints directory (with some changes)
+# create csv files for TimeFrame annotations in golds/timeframes
 
 
-from pathlib import Path
-import numpy as np
 import re
 import shutil
+import csv
+from pathlib import Path
+import numpy as np
 import pandas as pd
+
 
 def truncate(value, is_total = False):
     """
@@ -22,6 +25,8 @@ def truncate(value, is_total = False):
         # remove file extension
         truncated = truncated[:-4]
     return truncated
+
+
 def convert_ISO(value, is_total):
     """
     This method takes in a string of milliseconds and then converts the milliseconds to
@@ -40,35 +45,80 @@ def convert_ISO(value, is_total):
     return timestamp
 
 
-folder = Path.cwd()
-goldpath = folder / 'golds'
-# iterate through files in scene-recognition
-# if a directory starts w/ yy-mm-dd pattern, copy contents to the golds directory
-for directory in folder.glob('*'):
-    if directory.is_dir() and re.match("[0-9]{2}[0-1][0-9][0-3][0-9]", directory.name):
-        # copy contents by iterating
-        for file in directory.glob('*'):
-            source = file
-            destination = goldpath / file.name
-            shutil.copy2(source, destination)
-            # read in as dataframe to more easily manipulate columns
-            df = pd.read_csv(destination)
-            # create new timestamp column and fill with values
-            df.insert(1, 'timestamp', "")
-            df['timestamp'] = df['filename'].apply(convert_ISO, is_total=False)
-            # add total column (total_ms, second to last set of numbers in filename)
-            df.insert(2, 'total', df['filename'].apply(convert_ISO, is_total=True))
-            # remove unseen
-            df = df[(df.seen != "false") & (df.seen != "False")]
-            # remove seen column
-            df = df.drop('seen', axis=1)
-            # any that are left have been seen. therefore, any rows with label = "" are negative
-            # so their labels should be changed to "-"
-            df.loc[df['type label'].isna(), 'type label'] = '-'
-            # remove first column (filename)
-            df = df.drop('filename', axis=1)
-            # remove transcript and note columns
-            df = df.drop('transcript', axis=1)
-            df = df.drop('note', axis=1)
-            # output to csv with same filename
-            df.to_csv(destination, index=False)
+def copy_timepoints(source, destination):
+    # read in as dataframe to more easily manipulate columns
+    df = pd.read_csv(source)
+    df = amend_dataframe(df)
+    # output to csv
+    df.to_csv(destination, index=False)
+
+
+def copy_timeframes(source, destination):
+    # read in as dataframe to more easily manipulate columns
+    df = pd.read_csv(source)
+    df = amend_dataframe(df)
+    current_label = None
+    current_sublabel = None
+    current_frame = []
+    frames = []
+    with open(destination, "w") as csv_file:
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerow(['start', 'end', 'type label', 'subtype label', 'modifier'])
+        for index, row in df.iterrows():
+            ts = row['timestamp']
+            label = row['type label']
+            sublabel = row['subtype label']
+            sublabel = '-' if type(sublabel) is float else sublabel
+            modifier = row['modifier']
+            if current_label == label and current_sublabel == sublabel:
+                current_frame.append((ts, label, sublabel))
+            else:
+                if current_frame and current_frame[0][1] != '-':
+                    # this takes the modifier of the last timepoint
+                    timeframe = (current_frame[0][0], current_frame[-1][0],
+                                 current_label, current_sublabel, modifier)
+                    frames.append(timeframe)
+                    csv_writer.writerow(timeframe)
+                current_frame = []
+                current_label = label
+                current_sublabel = sublabel
+
+
+def amend_dataframe(df):
+    # create new timestamp column and fill with values
+    df.insert(1, 'timestamp', "")
+    df['timestamp'] = df['filename'].apply(convert_ISO, is_total=False)
+    # add total column (total_ms, second to last set of numbers in filename)
+    df.insert(2, 'total', df['filename'].apply(convert_ISO, is_total=True))
+    # remove unseen
+    df = df[(df.seen != "false") & (df.seen != "False")]
+    # remove seen column
+    df = df.drop('seen', axis=1)
+    # any that are left have been seen. therefore, any rows with label = "" are negative
+    # so their labels should be changed to "-"
+    df.loc[df['type label'].isna(), 'type label'] = '-'
+    # remove first column (filename)
+    df = df.drop('filename', axis=1)
+    # remove transcript and note columns
+    df = df.drop('transcript', axis=1)
+    df = df.drop('note', axis=1)
+    return df
+
+
+if __name__ == '__main__':
+
+    folder = Path.cwd()
+    goldpath = folder / 'golds'
+    # iterate through files in scene-recognition
+    # if a directory starts w/ yy-mm-dd pattern, copy contents to the golds directory
+    for directory in folder.glob('*'):
+        if directory.is_dir() and re.match("[0-9]{2}[0-1][0-9][0-3][0-9]", directory.name):
+            # copy contents by iterating
+            print(directory.name)
+            for file in directory.glob('*'):
+                #print('   ', file.name)
+                source = file
+                destination_points = goldpath / 'timepoints' / file.name
+                destination_frames = goldpath / 'timeframes' / file.name
+                copy_timepoints(source, destination_points)
+                copy_timeframes(source, destination_frames)
