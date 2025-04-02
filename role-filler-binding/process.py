a73900cc6002d2be5d525203587607e1d2f07bbb
Keigh Rim <keigh.rim@gmail.com>
2025-04-02 14:53:02 -0400

    changed gold format of rfb gold files 1) use iso timeformat, 2) better k-v pair format

--- role-filler-binding/process.py
+++ role-filler-binding/process.py
@@ -2,17 +2,20 @@ import csv
 import json
 import pathlib
 import shutil
+from mmif.utils import timeunit_helper as tuh
 
 from typing import Dict, Tuple
+
+FPS = 29.97  # we know all videos in aapb-ann-44 batch are encoded with 29.97 fps. For a future (very unlikley) event of adding more batches to this RFB work, this might need to be double-checked
 
 
 def build_csv_string(annos: Dict) -> Tuple[bool, str]:
     pruned_annos = {k: v for k, v in annos.items() if k and k[0] != "_" and v}
     is_skipped = False
 
-    out = "\n".join(
+    out = "|".join(
         [
-            f",{role},{filler}"
+            f"{role}={filler}"
             for role, fillers in pruned_annos.items()
             for filler in fillers
         ]
@@ -35,16 +38,17 @@ def process(raw_dir, golds_dir):
         annotations = json.load(open(fname, "r", encoding='utf-8'))
         out = []
         for frame, annotation in annotations.items():
+            timecode = tuh._second_to_isoformat(int(frame) / FPS)
             csv_line = (
-                frame,
+                timecode,
                 swt_type,
                 *build_csv_string(annotation)
             )
             out.append(csv_line)
-        outfname = golds_dir / fname.with_suffix('.gold.csv').name
+        outfname = golds_dir / fname.with_suffix('.csv').name
         with open(outfname, "w", encoding='utf-8') as f:
             writer = csv.writer(f, lineterminator='\n')
-            writer.writerow(["at", "scene-type", "SKIPPED", "ANNOTATIONS"])
+            writer.writerow(["at", "scene-type", "skipped", "role-fillers"])
             writer.writerows(out)
 
 
