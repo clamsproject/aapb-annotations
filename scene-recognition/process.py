dbbdc4318231ecd94e80d36e61b04c6fd7fd0a48
Marc Verhagen <marc@cs.brandeis.edu>
2025-04-14 12:03:59 -0400

    Reverted process script to old structure and changed column names

--- scene-recognition/process.py
+++ scene-recognition/process.py
@@ -1,6 +1,7 @@
-# process script to copy raw csv into new golds directory
-# iterate through each raw directory (use yy-mm-dd pattern to find directories)
-# copy each csv file to golds/timepoints directory (with some changes)
+# Process script to copy raw csv into new golds directory
+# 
+# - iterate through each raw directory (use yy-mm-dd pattern to find directories)
+# - copy each csv file to golds/timepoints directory (with some changes)
 # create csv files for TimeFrame annotations in golds/timeframes
 
 
@@ -52,15 +53,38 @@ def convert_ISO(value, is_total):
 
 
 def copy_timepoints(source, destination):
-    # read in as dataframe to more easily manipulate columns
+    """Copy time points to golds directory after some changes."""
     df = pd.read_csv(source)
     df = amend_dataframe(df)
     # output to csv
     df.to_csv(destination, index=False)
 
 
+def amend_dataframe(df):
+    # create new timestamp column and fill with values
+    df.insert(1, 'at', "")
+    df['at'] = df['filename'].apply(convert_ISO, is_total=False)
+    # Remove unseen rows and the seen column
+    # Note: as of April 2025 this does not appear to ever happen
+    df = df[(df.seen != "false") & (df.seen != "False")]
+    df = df.drop('seen', axis=1)
+    # Any rows that are left have been seen, therefore, any rows with label = "" are
+    # negative so their labels should be changed to "-"
+    df.loc[df['type label'].isna(), 'type label'] = '-'
+    # add a scene type column which gets the type label from the original data frame
+    df.insert(2, 'scene-type', "")
+    df['scene-type'] = df['type label']
+    # remove old type label column, remove filename, transcript and note columns
+    df = df.drop('type label', axis=1)
+    df = df.drop('filename', axis=1)
+    df = df.drop('transcript', axis=1)
+    df = df.drop('note', axis=1)
+    return df
+
+
 def copy_timeframes(source, destination):
-    # read in as dataframe to more easily manipulate columns
+    """This was used at some point to create timeframes from timepoints, but we are not
+    storing derivable data anymore. Keeping it around for a while in case it is useful."""
     df = pd.read_csv(source)
     df = amend_dataframe(df)
     current_label = None
@@ -68,19 +92,19 @@ def copy_timeframes(source, destination):
     current_frame = []
     with open(destination, "w") as csv_file:
         csv_writer = csv.writer(csv_file)
-        csv_writer.writerow(get_column_names())
+        csv_writer.writerow(_get_column_names())
         for index, row in df.iterrows():
             ts = row['timestamp']
             label = row['type label']
             sublabel = row['subtype label']
             sublabel = '-' if type(sublabel) is float else sublabel
             modifier = row['modifier']
-            if labels_match(current_label, label, current_sublabel, sublabel):
+            if _labels_match(current_label, label, current_sublabel, sublabel):
                 current_frame.append((ts, label, sublabel))
             else:
                 if current_frame and current_frame[0][1] != '-':
                     # this takes the modifier of the last timepoint
-                    timeframe = create_timeframe(
+                    timeframe = _create_timeframe(
                         current_frame, current_label, current_sublabel, modifier)
                     csv_writer.writerow(timeframe)
                 current_frame = [(ts, label, sublabel)]
@@ -88,41 +112,23 @@ def copy_timeframes(source, destination):
                 current_sublabel = sublabel
 
 
-def amend_dataframe(df):
-    # create new timestamp column and fill with values
-    df.insert(1, 'timestamp', "")
-    df['timestamp'] = df['filename'].apply(convert_ISO, is_total=False)
-    # add total column (total_ms, second to last set of numbers in filename)
-    df.insert(2, 'total', df['filename'].apply(convert_ISO, is_total=True))
-    # remove unseen
-    df = df[(df.seen != "false") & (df.seen != "False")]
-    # remove seen column
-    df = df.drop('seen', axis=1)
-    # any that are left have been seen. therefore, any rows with label = "" are negative
-    # so their labels should be changed to "-"
-    df.loc[df['type label'].isna(), 'type label'] = '-'
-    # remove first column (filename)
-    df = df.drop('filename', axis=1)
-    # remove transcript and note columns
-    df = df.drop('transcript', axis=1)
-    df = df.drop('note', axis=1)
-    return df
-
-
-def get_column_names():
+def _get_column_names():
+    """Helper method for copy_timeframes."""
     if USE_SUBLABEL:
         return ['start', 'end', 'type label', 'subtype label', 'modifier']
     return ['start', 'end', 'type label', 'modifier']
 
 
-def labels_match(current_label, label, current_sublabel, sublabel):
+def _labels_match(current_label, label, current_sublabel, sublabel):
+    """Helper method for copy_timeframes."""
     if USE_SUBLABEL:
         return current_label == label and current_sublabel == sublabel
     else:
         return current_label == label
 
 
-def create_timeframe(frame, label, sublabel, modifier):
+def _create_timeframe(frame, label, sublabel, modifier):
+    """Helper method for copy_timeframes."""
     sub = [sublabel] if USE_SUBLABEL else []
     return [frame[0][0], frame[-1][0], label] + sub + [modifier]
 
@@ -132,16 +138,12 @@ if __name__ == '__main__':
 
     folder = Path.cwd()
     goldpath = folder / 'golds'
-    # iterate through files in scene-recognition
-    # if a directory starts w/ yy-mm-dd pattern, copy contents to the golds directory
     for directory in folder.glob('*'):
+        # assume that directories starting with yy-mm-dd pattern are batch directories
         if directory.is_dir() and re.match("[0-9]{2}[0-1][0-9][0-3][0-9]", directory.name):
-            # copy contents by iterating
             print(directory.name)
             for file in directory.glob('*'):
                 print('   ', file.name)
                 source = file
-                destination_points = goldpath / 'timepoints' / file.name
-                destination_frames = goldpath / 'timeframes' / file.name
-                copy_timepoints(source, destination_points)
-                copy_timeframes(source, destination_frames)
+                destination = goldpath / file.name
+                copy_timepoints(source, destination)
